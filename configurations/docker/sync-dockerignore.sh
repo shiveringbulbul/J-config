2b371bf1c0d35fd7ed4413e33f28a079bd9401d3
JackaL <shiveringbulbul@hotmail.com>
2020-12-08 16:26:38 +0800

    .

--- configurations/docker/sync-dockerignore.sh
+++ configurations/docker/sync-dockerignore.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
+echo =======================
+echo == sync-dockerignore ==
+echo =======================
+trapper() { echo =======================; echo; echo; }; trap trapper EXIT
 
-# .gitignore should be in the parent directory of this script
+
+# cd into root dir
 DIR=$(dirname "$(readlink -f "$0")")
 cd $DIR && cd ..
 
-# writing
+
+# .gitignore should be in the parent directory of this script
 cat .gitignore > .dockerignore
 if cat $DIR/.dockerignore.src >> .dockerignore
 then
-  echo "sync succeeded"
+  echo "Sync succeeded!"
 else
-  echo "sync failed"
+  echo "Sync failed!"
 fi
 
 
@@ -18,7 +24,9 @@ fi
 git status --porcelain | grep 'M  .dockerignore' > /dev/null
 if [ $? = 0 ]
 then
+  echo "However, you have modified the file, and need a new git commit."
   exit 1
 else
+  echo "And All is Well!"
   exit 0
 fi
